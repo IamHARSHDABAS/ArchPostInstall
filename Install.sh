238213157bd58dae2cfd920f54362618063683bb
Harsh Dabas <iamharshdabas@gmail.com>
2022-12-17 15:11:21 +0530

    Removed i3status && added polybar

--- Install.sh
+++ Install.sh
@@ -2,7 +2,7 @@
 
 # Install Packages
 sudo pacman -Syyu
-sudo pacman -S alacritty bluez bluez-utils bridge-utils brightnessctl dnsmasq feh firefox gimp git jdk-openjdk jre-openjdk libguestfs libreoffice-fresh maim neofetch neovim openbsd-netcat qemu ranger spotify-launcher telegram-desktop unzip vde2 virt-manager virt-viewer zip
+sudo pacman -S alacritty bluez bluez-utils bridge-utils brightnessctl dnsmasq feh firefox gimp git jdk-openjdk jre-openjdk libguestfs libreoffice-fresh maim neofetch neovim polybar openbsd-netcat qemu ranger spotify-launcher telegram-desktop unzip vde2 virt-manager virt-viewer zip
 
 # Bluetooth
 sudo systemctl enable bluetooth
@@ -16,7 +16,7 @@ sudo usermod -aG libvirt $USER
 # make directory
 mkdir ~/.config/alacritty
 mkdir ~/.config/i3
-mkdir ~/.config/i3status
+mkdir ~/.config/polybar
 mkdir ~/Gimp
 mkdir ~/Linux
 mkdir ~/Office
@@ -29,7 +29,8 @@ sudo mkdir /etc/libvirt
 # Configs
 cp DotFiles/Alacritty/alacritty.yml ~/.config/alacritty/
 cp DotFiles/I3/config ~/.config/i3/
-cp DotFiles/I3Status/config ~/.config/i3status/
+cp DotFiles/Polybar/config.ini ~/.config/polybar/
+cp DotFiles/Polybar/launch.sh ~/.config/polybar/
 sudo cp DotFiles/Libvirt/libvirtd.conf /etc/libvirt/
 cp Wallpaper/596610.jpg ~/Wallpaper/
 
