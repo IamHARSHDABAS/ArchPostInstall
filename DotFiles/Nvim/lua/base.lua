9e1a760571d06fd7c71c9c6bd8a170937d59b6ab
Harsh Dabas <iamharshdabas@gmail.com>
2023-01-03 13:12:54 +0530

    Added nvim config

---
+++ DotFiles/Nvim/lua/base.lua
@@ -0,0 +1,50 @@
+local g = vim.g
+local o = vim.o
+local opt = vim.opt
+
+-- Use Terminal Colors
+o.termguicolors = true
+
+-- Number of screen lines to keep above and below the cursor
+o.scrolloff = 10
+
+-- Better editor UI
+o.number = true
+o.relativenumber = true
+
+-- Better editing experience
+o.expandtab = true
+o.smarttab = true
+o.cindent = true
+o.autoindent = true
+o.wrap = true
+o.textwidth = 300
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = -1 -- If negative, shiftwidth value is used
+o.list = true
+o.listchars = "trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂"
+-- o.listchars = 'eol:¬,space:·,lead: ,trail:·,nbsp:◇,tab:→-,extends:▸,precedes:◂,multispace:···⬝,leadmultispace:│   ,'
+-- o.formatoptions = 'qrn1'
+
+-- Makes neovim and host OS clipboard play nicely with each other
+o.clipboard = "unnamedplus"
+
+-- Case insensitive searching UNLESS /C or capital in search
+o.ignorecase = true
+o.smartcase = true
+
+-- Undo and backup options
+o.backup = false
+o.writebackup = false
+o.undofile = true
+o.swapfile = false
+
+-- Better buffer splitting
+o.splitright = true
+o.splitbelow = true
+
+-- Map <leader> to space
+g.mapleader = " "
+g.maplocalleader = " "
+
