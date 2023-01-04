d1e84a987a63ae804e0bde95b2645e64760c661a
Harsh Dabas <iamharshdabas@gmail.com>
2023-01-04 12:11:29 +0530

    Reworked

--- Install.sh
+++ Install.sh
@@ -14,46 +14,47 @@ sudo systemctl start libvirtd
 sudo usermod -aG libvirt $USER
 
 # make directory
-mkdir ~/.config/alacritty
-mkdir ~/.config/polybar
-mkdir ~/.config/rofi
-mkdir ~/.config/nvim
-mkdir ~/Archive
-mkdir ~/Java
-mkdir ~/Gimp
-mkdir ~/Linux
-mkdir ~/Office
-mkdir ~/Screenshot
-mkdir ~/Telegram
-mkdir ~/Temp
-mkdir ~/Wallpaper
-mkdir ~/Whatsapp
+mkdir $HOME/Archive/
+mkdir $HOME/Java/
+mkdir $HOME/Gimp/
+mkdir $HOME/Linux/
+mkdir $HOME/Office/
+mkdir $HOME/Screenshot/
+mkdir $HOME/Telegram/
+mkdir $HOME/Temp/
+mkdir $HOME/Wallpaper/
+mkdir $HOME/Whatsapp/
+mkdir -p $HOME/.config/alacritty/
+mkdir -p $HOME/.config/nvim/
+mkdir -p $HOME/.config/polybar/
+mkdir -p $HOME/.config/rofi/
 
 # Configs
-cp DotFiles/Alacritty/alacritty.yml ~/.config/alacritty
-cp DotFiles/I3/config ~/.config/i3
-cp DotFiles/Polybar/config.ini ~/.config/polybar
-cp DotFiles/Polybar/launch.sh ~/.config/polybar
-cp DotFiles/Rofi/arthur.rasi ~/.config/rofi
-cp DotFiles/Rofi/config.rasi ~/.config/rofi
-cp Wallpaper/wallhaven-2evx9g.jpg ~/Wallpaper
-cp -r DotFiles/Nvim/* ~/.config/nvim
-sudo cp DotFiles/Libvirt/libvirtd.conf /etc/libvirt
+cp -r DotFiles/Alacritty/* $HOME/.config/alacritty/
+cp -r DotFiles/I3/* $HOME/.config/i3/
+cp -r DotFiles/Nvim/* $HOME/.config/nvim/
+cp -r DotFiles/Polybar/* $HOME/.config/polybar/
+cp -r DotFiles/Rofi/* $HOME/.config/rofi/
+cp -r Wallpaper/* $HOME/Wallpaper/
+sudo cp DotFiles/Libvirt/libvirtd.conf /etc/libvirt/
 
 # Nvim
-mkdir -p ~/.local/share/nvim/site/pack/packer/start
-cd ~/.local/share/nvim/site/pack/packer/start
-git clone --depth 1 https://github.com/wbthomason/packer.nvim
-cd ~
+mkdir -p $HOME/.local/share/nvim/site/pack/packer/start/
+cd $HOME/.local/share/nvim/site/pack/packer/start/
+git clone https://github.com/wbthomason/packer.nvim
+cd $HOME/
 
 # Aur Helper
-cd ~
+cd $HOME/
 git clone https://aur.archlinux.org/yay.git
-cd yay
+cd yay/
 makepkg -si
-cd ~
+cd $HOME/
 
 # Build Packages
 yay whatsapp-for-linux
 yay tldr
 yay ttyper
+
+# Reboot
+reboot
