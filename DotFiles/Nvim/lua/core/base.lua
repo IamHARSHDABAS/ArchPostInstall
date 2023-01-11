1d5becfd3d11d2503748aab9265ad8a5411ad710
Harsh Dabas <iamharshdabas@gmail.com>
2023-01-11 18:25:37 +0530

    Its just better now

--- DotFiles/Nvim/lua/core/base.lua
+++ DotFiles/Nvim/lua/core/base.lua
@@ -1,21 +1,11 @@
 local g = vim.g
 local o = vim.o
 local opt = vim.opt
-
--- Terminal Colors
 o.termguicolors = true
-
--- Scroll limit
-o.scrolloff = 5
-
--- Number line
+o.scrolloff = 10
 o.number = true
 o.relativenumber = true
-
--- Tabs
 o.tabstop = 4
 o.shiftwidth = 4
-
--- Indicate space
 o.list = true
 o.listchars = "trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂"
--- DotFiles/Nvim/lua/core/plugin-config/lualine.lua
+++ DotFiles/Nvim/lua/core/plugin-config/lualine.lua
@@ -1,6 +1,12 @@
+-- Eviline config for lualine
+-- Author: shadmansaleh
+-- Credit: glepnir
 local lualine = require('lualine')
+
+-- Color table for highlights
+-- stylua: ignore
 local colors = {
-  bg       = '#000000',
+  bg       = '#202328',
   fg       = '#bbc2cf',
   yellow   = '#ECBE7B',
   cyan     = '#008080',
