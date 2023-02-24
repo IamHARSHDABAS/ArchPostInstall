7625a626451c0ea3b20c4f4b448f2fe2529b2d13
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-02-24 20:03:44 +0530

    Rm trash

--- Install.sh
+++ Install.sh
@@ -1,53 +1,20 @@
 #!/bin/bash
 
 # Install Packages
-sudo pacman -Syyu
-sudo pacman -S alacritty bluez bluez-utils bridge-utils brightnessctl dnsmasq feh firefox flatpak gimp git jdk-openjdk jre-openjdk libguestfs libreoffice-fresh lsd maim neofetch neovim nodejs npm polybar python python-pip qemu-base ranger ripgrep rofi spotify-launcher telegram-desktop tree ttf-firacode-nerd unzip vde2 virt-manager virt-viewer xsel zip
+sudo pacman -S alacritty bluez bluez-utils brightnessctl feh firefox gimp git jdk-openjdk libreoffice-fresh lsd neofetch neovim npm python-pip ranger ripgrep spotify-launcher telegram-desktop tree ttf-firacode-nerd unzip zip
 
 # Bluetooth
 sudo systemctl enable bluetooth
 sudo systemctl start bluetooth
 
-# Virt-manager
-sudo systemctl enable libvirtd
-sudo systemctl start libvirtd
-sudo usermod -aG libvirt $USER
-
 # Neovim
 pip install pynvim
-sudo npm i -g Neovim
+# sudo npm i -g Neovim  # ERROR 404
 
 # My Repos
 git clone https://github.com/IamHARSHDABAS/C
 git clone https://github.com/IamHARSHDABAS/DotFiles
-git clone https://github.com/IamHARSHDABAS/IamHARSHDABAS
 git clone https://github.com/IamHARSHDABAS/Java
 git clone https://github.com/IamHARSHDABAS/NeoVim
 git clone https://github.com/IamHARSHDABAS/Sweetify
-git clone https://github.com/IamHARSHDABAS/SweetifyNeoVim
-
-# make directory
-mkdir -p $HOME/Screenshot/
-mkdir -p $HOME/Telegram/
-mkdir -p $HOME/Temporary/
-mkdir -p $HOME/Wallpaper/
-mkdir -p $HOME/Whatsapp/
-mkdir -p $HOME/.config/alacritty/
-mkdir -p $HOME/.config/nvim/
-mkdir -p $HOME/.config/polybar/
-mkdir -p $HOME/.config/rofi/
-mkdir -p $HOME/.config/tealdeer/
-
-# Configs
-cp -r $HOME/DotFiles/Alacritty/* $HOME/.config/alacritty/
-cp -r $HOME/DotFiles/Bash/* $HOME/.bashrc
-cp -r $HOME/DotFiles/I3/* $HOME/.config/i3/
-cp -r $HOME/DotFiles/Polybar/* $HOME/.config/polybar/
-cp -r $HOME/DotFiles/Rofi/* $HOME/.config/rofi/
-cp -r $HOME/DotFiles/Tealdeer/* $HOME/.config/tealdeer/
-cp -r $HOME/NeoVim/* $HOME/.config/nvim/
-cp -r I3PostInstall/Wallpaper/* $HOME/Wallpaper/
-sudo cp -r $HOME/DotFiles/Libvirt/libvirtd.conf /etc/libvirt/
-
-# Flatpak
-flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community
+git clone https://github.com/IamHARSHDABAS/ClickCounter
